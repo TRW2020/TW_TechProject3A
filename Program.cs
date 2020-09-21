0be0f3c1c27e8a5dee42a14c8bdd4d3eaa8f2b46
Tyler W <twildrick@usf.edu>
2020-09-21 02:14:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TW_TechProject3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
